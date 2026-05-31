3b790ada09d820eeda91940f2d1680817affec6b
Sachin Bind <sachinbind7977@gmail.com>
2026-05-31 16:05:17 +0530

    fix admin login redirect issue

--- src/infrastructure/api.js
+++ src/infrastructure/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3000";
+const baseURL = 'https://laundry-backend-7vcd.onrender.com' || "http://localhost:3000";
 
 const api = axios.create({
   baseURL,
