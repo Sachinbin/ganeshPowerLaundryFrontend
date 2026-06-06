2323a776e8b8eed2df9cfe0785eabc8b54c484cf
Sachin Bind <sachinbind7977@gmail.com>
2026-06-06 20:37:17 +0530

    apiurl changed

--- src/infrastructure/api.js
+++ src/infrastructure/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = 'https://laundry-backend-wdek.onrender.com' || "http://localhost:3000";
+const baseURL ="https://laundry-backend-d260.onrender.com" || "http://localhost:3000";
 
 const api = axios.create({
   baseURL,
